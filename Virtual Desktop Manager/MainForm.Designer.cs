1ab324f12a6ffae6023c25b3862414c1d9a5ffd0
Nelson Gomes Neto <ngomesneto@outlook.com>
2020-09-01 22:02:39 -0300

    Add Virtual Desktop Manager vALPHA - Basic Ctrl + Num Virtual Desktop Switcher (binded to Ctrl + Win + Alt + Num)

---
+++ Virtual Desktop Manager/MainForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Virtual_Desktop_Manager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.virtualDesktopCountLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.EnableButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // virtualDesktopCountLabel
+            // 
+            this.virtualDesktopCountLabel.AutoSize = true;
+            this.virtualDesktopCountLabel.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.virtualDesktopCountLabel.Location = new System.Drawing.Point(314, 9);
+            this.virtualDesktopCountLabel.Name = "virtualDesktopCountLabel";
+            this.virtualDesktopCountLabel.Size = new System.Drawing.Size(32, 37);
+            this.virtualDesktopCountLabel.TabIndex = 0;
+            this.virtualDesktopCountLabel.Text = "0";
+            this.virtualDesktopCountLabel.Click += new System.EventHandler(this.virtualDesktopCountLabel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(296, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Virtual Desktops Count:";
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Virtual Desktop Manager";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // EnableButton
+            // 
+            this.EnableButton.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.EnableButton.Location = new System.Drawing.Point(12, 49);
+            this.EnableButton.Name = "EnableButton";
+            this.EnableButton.Size = new System.Drawing.Size(330, 56);
+            this.EnableButton.TabIndex = 2;
+            this.EnableButton.Text = "Disable";
+            this.EnableButton.UseVisualStyleBackColor = true;
+            this.EnableButton.Click += new System.EventHandler(this.EnableButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 123);
+            this.Controls.Add(this.EnableButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.virtualDesktopCountLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Virtual Desktop Manager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label virtualDesktopCountLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Button EnableButton;
+    }
+}
+
---
+++ Virtual Desktop Manager/MainForm.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using WindowsDesktop;
+
+namespace Virtual_Desktop_Manager
+{
+    public partial class MainForm : Form
+    {
+        private KeyHandler[] ghks = new KeyHandler[10];
+        private VirtualDesktop[] virtualDesktops;
+        private bool activated = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            EnableButton.BackColor = Color.Red;
+
+			InitializeComObjects();
+            for (int i = 0; i < 10; i++)
+                ghks[i] = new KeyHandler(Constants.WIN + Constants.CTRL + Constants.ALT, Keys.D0 + i, this);
+            InitializeKeyHandlers();
+
+			RefreshVirtualDesktops();
+		}
+
+		private static async void InitializeComObjects()
+		{
+			try
+			{
+				await VirtualDesktopProvider.Default.Initialize(TaskScheduler.FromCurrentSynchronizationContext());
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Failed to initialize.");
+			}
+
+			VirtualDesktop.CurrentChanged += (sender, args) => System.Diagnostics.Debug.WriteLine($"Desktop changed: {args.NewDesktop.Id}");
+		}
+
+        private void InitializeKeyHandlers()
+        {
+            for (int i = 0; i < 10; i++)
+                ghks[i].Register();
+            EnableButton.BackColor = Color.Green;
+            EnableButton.Text = "Disable";
+            activated = true;
+        }
+
+        private void RemoveAllKeyHandlers()
+        {
+            for (int i = 0; i < ghks.Length; i++)
+                if (!ghks[i].Unregiser())
+                    MessageBox.Show("Hotkey failed to unregister!");
+            EnableButton.BackColor = Color.Red;
+            EnableButton.Text = "Enable";
+            activated = false;
+        }
+
+        private void HandleHotkey(Keys key, int modifier)
+        {
+            if (key == Keys.D0)
+                virtualDesktops.Last().Switch();
+            else if (key - Keys.D1 < virtualDesktops.Length)
+                virtualDesktops[key - Keys.D1].Switch();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Constants.WM_HOTKEY_MSG_ID)
+            {
+                Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
+                int modifier = (int)m.LParam & 0xFFFF;
+                HandleHotkey(key, modifier);
+            }
+            
+            base.WndProc(ref m);
+        }
+
+        private void RefreshVirtualDesktops()
+        {
+			virtualDesktops = VirtualDesktop.GetDesktops();
+			virtualDesktopCountLabel.Text = virtualDesktops.Length.ToString();
+		}
+
+        private void virtualDesktopCountLabel_Click(object sender, EventArgs e)
+        {
+			RefreshVirtualDesktops();
+		}
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+			RefreshVirtualDesktops();
+		}
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            RemoveAllKeyHandlers();
+        }
+
+        private void EnableButton_Click(object sender, EventArgs e)
+        {
+            if (activated)
+                RemoveAllKeyHandlers();
+            else
+                InitializeKeyHandlers();
+        }
+    }
+
+    public class KeyHandler
+    {
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private int modifier;
+        private int key;
+        private IntPtr hWnd;
+        private int id;
+
+        public KeyHandler(int modifier, Keys key, Form form)
+        {
+            this.modifier = modifier;
+            this.key = (int)key;
+            this.hWnd = form.Handle;
+            id = this.GetHashCode();
+        }
+
+        public override int GetHashCode()
+        {
+            return modifier ^ key ^ hWnd.ToInt32();
+        }
+
+        public bool Register()
+        {
+            return RegisterHotKey(hWnd, id, modifier, key);
+        }
+
+        public bool Unregiser()
+        {
+            return UnregisterHotKey(hWnd, id);
+        }
+    }
+
+    public static class Constants
+	{
+        // modifiers
+        public const int NOMOD = 0x0000;
+        public const int ALT = 0x0001;
+        public const int CTRL = 0x0002;
+        public const int SHIFT = 0x0004;
+        public const int WIN = 0x0008;
+
+        // windows message id for hotkey
+        public const int WM_HOTKEY_MSG_ID = 0x0312;
+	}
+}
